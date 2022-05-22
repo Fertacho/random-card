4eeb4ba4795c55bd9c33bd7c3310737ea8ee8274
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-22 19:51:48 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -3,3 +3,26 @@ import "./style.css";
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
+
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let suits = ["diamond", "heart", "club", "spade"];
+
+function randomcard(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+let numeros = randomcard(numbers);
+let pintas = randomcard(suits);
