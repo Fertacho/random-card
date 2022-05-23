29d90e5e657787587e1a777856d63774a91d92e2
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-23 00:08:22 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -1,31 +1,32 @@
 import "./style.css";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let suits = ["diamond", "heart", "club", "spade"];
+
+  let red = ["diamond", "heart"];
+
+  function randomcard(items) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+  let numeros = randomcard(numbers);
+  let pintas = randomcard(suits);
+
+  console.log(numeros);
+  console.log(pintas);
 };
-
-let numbers = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-let suits = ["diamond", "heart", "club", "spade"];
-
-function randomcard(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-let numeros = randomcard(numbers);
-let pintas = randomcard(suits);
-
-console.log(numeros);
-console.log(pintas);
